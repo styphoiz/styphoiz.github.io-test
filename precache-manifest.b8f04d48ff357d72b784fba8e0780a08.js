377d32db6c048882c7e2999b69c7da54fb476745
styphoiz <styphoiz@gmail.com>
2020-08-05 12:41:30 +0200

    Updates

---
+++ precache-manifest.b8f04d48ff357d72b784fba8e0780a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542dfc3886b4c316402fbe0cb3aec3d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1252d99d8583198c333",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9d82b108e626ab1ced6",
+    "url": "/static/js/2.bd5fb903.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd5fb903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1252d99d8583198c333",
+    "url": "/static/js/main.53cd91b9.chunk.js"
+  },
+  {
+    "revision": "f438879c68bfc8864397",
+    "url": "/static/js/runtime-main.cc36b19e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
